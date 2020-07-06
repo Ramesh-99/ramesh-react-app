f59b4a091fdecb9ac2fa40cfcdadd6d8aa99b26d
Ramesh-99 <rkumar95345@gmail.com>
2020-07-06 11:41:13 +0530

    first commit

---
+++ src/components/quizData.js
@@ -0,0 +1,26 @@
+export const quizData = [
+  {
+    id: 0,
+    question: "Which of these is not a programming language?",
+    options: ['JavaScript', 'Java', 'NodeJS', 'TypeScript'],
+    answer: "NodeJS"
+  },
+  {
+    id: 1,
+    question: "Which of the following is not a valid Javascipt variable name ?",
+    options: ['var1', 'var_1', 'Var1', 'var-1'],
+    answer:  "var-1"
+  },
+  {
+    id: 3,
+    question: "Who is the creator of JavaScript?",
+    options: ['Brendan Eich', 'Guido van Rossum', 'Dennis Ritchie', 'James Gosling'],
+    answer: "Brendan Eich"
+  },
+  {
+    id: 4,
+    question: "What is the type of 'NaN' in JavaScript ?",
+    options: ['object', 'number', 'undefined', 'other'],
+    answer:"number"
+  }
+];
